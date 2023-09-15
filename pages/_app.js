3035a319632d7dc1aa70c3569e59bd3e43f94624
Sepehr Tavakoli <sepehrtvk2001@gmail.com>
2023-09-15 18:55:27 +0330

    add bootstrap and scss

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
-import '../styles/globals.css'
+// // add bootstrap css
+// import 'bootstrap/dist/css/bootstrap.css';
+
+// own css files here
+import '../styles/scss/global.scss';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
