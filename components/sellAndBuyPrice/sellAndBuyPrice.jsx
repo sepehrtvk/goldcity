c044150f77ef1ee764bfb0afbb4262aca0433c9e
Sepehr Tavakoli <sepehrtvk2001@gmail.com>
2023-09-16 17:35:03 +0330

    add buyGold

---
+++ components/sellAndBuyPrice/sellAndBuyPrice.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import {
+  convertNumbersToEnglish,
+  toLocaleCurrencyString,
+} from '../../common/Localization';
+
+const SellAndBuyPrice = ({ goldPrice, getValues }) => {
+  const [price, setPrice] = useState('');
+  const [amount, setAmount] = useState('');
+
+  useEffect(() => {
+    getValues({ price, amount });
+  }, [price, amount]);
+
+  return (
+    <div className='my-4'>
+      <div className='mb-3 d-flex align-items-center justify-content-center'>
+        <label htmlFor='amount' className='form-label mb-0 ms-3 fw-bold'>
+          مبلغ
+        </label>
+        <input
+          value={price && toLocaleCurrencyString(price)}
+          type='text'
+          className='form-control text-center'
+          id='amount'
+          style={{ width: 200 }}
+          onChange={(e) => {
+            const valueKey = e.target.value.replaceAll('٬', '');
+            const value = convertNumbersToEnglish(valueKey);
+            const re = /^[0-9\b]+$/;
+
+            if (value === '' || re.test(value)) {
+              setPrice(value);
+              setAmount((+value / goldPrice).toFixed(8).toString());
+            }
+          }}
+        />
+        <span className='me-2'>ریال</span>
+      </div>
+      <div className='mb-3 d-flex align-items-center justify-content-center'>
+        <label htmlFor='amount' className='form-label mb-0 ms-2 fw-bold'>
+          مقدار
+        </label>
+        <input
+          value={amount}
+          onChange={(e) => {
+            const value = e.target.value;
+
+            const re = /^[0-9\b]+$/;
+
+            if (value === '' || re.test(value)) {
+              setAmount(value);
+              setPrice((+value * goldPrice).toString());
+            }
+          }}
+          type='text'
+          className='form-control text-center'
+          style={{ width: 200 }}
+          id='amount'
+        />
+        <span className='me-2'>گرم</span>
+      </div>
+    </div>
+  );
+};
+
+export default SellAndBuyPrice;
--- pages/buyGoldFromUs/index.js
+++ pages/buyGoldFromUs/index.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { toLocaleCurrencyString } from '../../common/Localization';
+import SellAndBuyPrice from '../../components/sellAndBuyPrice/sellAndBuyPrice';
 
 const BuyGoldFromUs = () => {
-  return <div>buyGoldFromUs</div>;
+  const [values, setValues] = useState(null);
+
+  const getValues = (values) => {
+    setValues(values);
+  };
+
+  return (
+    <div className='row justify-content-center my-5'>
+      <div className='col-12 col-md-4'>
+        <div className='border border-primary rounded-2 p-3 bg-light'>
+          <div className='d-flex justify-content-between align-items-center'>
+            <div>
+              <span className='text-primary fw-bold'>خرید طلا از ما</span>
+            </div>
+            <div>
+              <span className='text-gold ms-2'>
+                {toLocaleCurrencyString('23700704')}
+              </span>
+              <span className='text-primary'>ریال</span>
+            </div>
+          </div>
+        </div>
+
+        <div className='border border-primary rounded-2 p-3 bg-light mt-4'>
+          <div className='d-flex flex-column justify-content-between '>
+            <SellAndBuyPrice goldPrice={23700704} getValues={getValues} />
+            <p className='mt-2'>
+              {`شما مقدار ${
+                values && values.amount ? values.amount : '0'
+              } گرم طلای خام دریافت خواهید نمود`}
+            </p>
+            <p>موجودی کیف پول شما 0 ریال است.</p>
+            <p style={{ textAlign: 'justify', lineHeight: '1.75rem' }}>
+              به دلیل نوسانات قیمت، پس از تایید اولیه و قبل از خرید نهایی، میزان
+              دقیق قابل خرید نمایش داده خواهد شد.
+            </p>
+          </div>
+        </div>
+        <div className='mt-4'>
+          <span className='btn btn-primary w-100'>مشاهده پیش فاکتور</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BuyGoldFromUs;
